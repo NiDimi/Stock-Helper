505a27a3fc287b484377190df7861e61c5eafcb5
moonkace24 <moonkace24@gmail.com>
2021-04-25 10:53:28 +0100

    Fixed stock getter and setter for dart

--- lib/models/stock.dart
+++ lib/models/stock.dart
@@ -1,27 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Stock {
-  String _name;
+  String name;
   final String ticker;
   final double price;
   final int quantity;
-  double _currentPrice;
+  double currentPrice;
 
   Stock({
     @required this.ticker,
     @required this.price,
     @required this.quantity,
   });
-
-  double get currentPrice => _currentPrice;
-
-  set currentPrice(double value) {
-    _currentPrice = value;
-  }
-
-  String get name => _name;
-
-  set name(String value) {
-    _name = value;
-  }
 }
